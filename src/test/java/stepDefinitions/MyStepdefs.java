467e6dd967718a16c514a1dfb1663af37f749bdf
Antonio Seraus <41804476+SerausAntonio@users.noreply.github.com>
2023-10-19 04:01:08 +0200

    third commit

--- src/test/java/stepDefinitions/MyStepdefs.java
+++ src/test/java/stepDefinitions/MyStepdefs.java
@@ -4,12 +4,17 @@ import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 
 public class MyStepdefs {
+    WebDriver driver;
     @Given("User is at the login page of the application")
     public void userIsAtTheLoginPageOfTheApplication() {
+        driver = new ChromeDriver();
+        driver.get("https://knipselkrant-curacao.com/");
         System.out.println("I am on the login page");
     }
 
